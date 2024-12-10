65e944e052fdd5df91a4c67a6feae6b6fb433cec
snehaltayade16 <snehaldeepaktayade@gmail.com>
2024-12-10 11:01:36 +0530

    update

---
+++ src/components/footer.jsx
@@ -0,0 +1,28 @@
+const Footer=()=>{
+    return(
+        <div className='absolute bg-black py-20 px-40 w-full'>
+            <div className='bg-gray-600 px-12'>
+                <div>
+                    <p className='text-white'>Questions ? call 000-800-919-1743</p>
+                </div>
+                <div className=''>
+                    <ul className='w-full inline-flex items-center justify-between'>
+                        <li>sfjshf</li>
+                        <li>sfjshf</li>
+                        <li>sfjshf</li>
+                        <li>sfjshf</li>
+                        <li>sfjshf</li>
+                        <li>sfjshf</li>
+                    </ul>
+                </div>
+                <div>
+                    <select>
+                        <option>English</option>
+                        <option>Hindi</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,18 +1,40 @@
-import Header from './header'
+import Header from './header';
+import Footer from './footer';
+
+
+
+
 const  Login =()=>{
     return(
-        <div>
-            <div className='absolute'>
-               <img src='https://assets.nflxext.com/ffe/siteui/vlv3/ce449112-3294-449a-b8d3-c4e1fdd7cff5/web/IN-en-20241202-TRIFECTA-perspective_0acfb303-6291-4ad1-806f-dda785f6295a_medium.jpg'  className="object-cover h-full w-full"></img>
+        <div className="relative">
+            <div className='absolute h-full w-full bg-black'>
+                <img src='https://assets.nflxext.com/ffe/siteui/vlv3/ce449112-3294-449a-b8d3-c4e1fdd7cff5/web/IN-en-20241202-TRIFECTA-perspective_0acfb303-6291-4ad1-806f-dda785f6295a_medium.jpg'  className="object-cover h-full w-full opacity-50"></img>
             </div>
-            <Header />
-            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-                <form className=" bg-black text-white p-10 max-w-md w-full mx-auto my-auto right-0 left-0">
-                    <input type="text" placeholder="Enter Email Address"></input>
-                    <input type="password" placeholder="Enter Password"></input>
-                    <button>Sign In</button>
-                </form>
+            <div className='relative'>
+                <Header />
+                <main className="pr-20 pl-20 pb-20 flex justify-center items-center">
+                    <form className="bg-[#020202c9] text-white py-12 px-16 flex flex-col w-1/4">
+                        <h1 className='text-4xl font-bold text-start'>Sign In</h1>
+                        <input type="text" placeholder="Enter Email or mobile number" className='mt-4 pt-6 pb-2 pr-4 pl-4 text-[#ffffff] bg-[#141311a3]'></input>
+                        <input type="password" placeholder="Enter Password" className='mt-4 pt-6 pb-2 pr-4 pl-4 text-[#ffffff] bg-[#141311a3]'></input>
+                        <button className='mt-4 bg-red-600 p-3 text-base font-medium rounded-md '>Sign In</button>
+                        <p className='text-center mt-4 text-[#ffffff80]'>OR</p>
+                        <button className='mt-4 p-3 bg-[#373337] text-base font-medium rounded-md '>Use Sign-In code</button>
+                        <p className='text-center mt-4 underline'>Forgot-passowrd?</p>
+                        <div>
+                            <span className='mt-4 text-[#ffffff] flex'>
+                                <input type="checkbox"/>
+                                <p className='text-[#ffffff] pl-3'>Remember me</p> 
+                            </span>
+                            <p className='mt-4 text-[#ffffff80]'>New to Netflix? <span className='text-white'>Sign up now.</span> </p> 
+                            <p className='mt-4 text-[#ffffff80] font-normal text-sm'>This page is protected by Google reCAPTCHA to ensure you're not a bot</p>
+                            <a className='mt-4 text-sm text-blue-600'>Learn more</a>
+                        </div>
+                        
+                    </form>
+                </main>
             </div>
+            <Footer />
         </div>
     )
 }
